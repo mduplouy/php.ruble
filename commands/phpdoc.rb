fbda0a2425ecd536a750b60d8e800f1dd778c608
Kevin Korb <kevinkorb@Kevin-Korbs-iMac-2.local>
2012-04-15 22:52:08 -0500

    Improved PHPDoc method generator

--- commands/phpdoc.rb
+++ commands/phpdoc.rb
@@ -1,29 +1,35 @@
 require 'ruble'
 
+@@COUNT = 1
+
 command 'Add PHPDoc For Method/Function' do |cmd|
-  cmd.key_binding = 'CONTROL+SHIFT+COMMAND+W'
+  cmd.key_binding = 'CONTROL+SHIFT+COMMAND+C'
   cmd.output = :insert_as_snippet
   cmd.input = :selection
   cmd.invoke do |context|
     input = STDIN.read
-    signature = input.scan(/\(([^\)]+)/)[0][0];
+    signature = input.scan(/\(([^\)]+)/)[0][0]
     methodName = input.scan(/function ([^\(]+)/)[0][0]
-    methodNameArray = methodName.strip().split("_");
-    methodDescription = "";
+    methodNameArray = methodName.strip().split("_")
+    methodDescription = ""
     methodNameArray.each do |namePart|
-      methodDescription += namePart.capitalize + " ";
+      methodDescription += namePart.capitalize + " "
     end
-    methodDescription = methodDescription.strip() + "\n";
+    methodDescription = methodDescription.strip()
     signatureStringSplit = signature.split(',');
     toPrint = "\t/**\n";
-    toPrint += "\t * " + "${1:" + methodDescription + "} \n"
+    toPrint += "\t * " + "${" + @@COUNT.to_s + ":" + methodDescription + "}\n"
+    @@COUNT += 1
+    toPrint += "\t *\n"
     signatureStringSplit.each do |myVar|
             myVar = myVar.strip().gsub!('$', '\$')
             varType = (myVar =~ /id$/) ? "integer" : "string"
-            toPrint += "\t * @param " + varType + " " + myVar + "\n";
+            toPrint += "\t * @param ${" + @@COUNT.to_s + ":" + varType + "} " + myVar + "\n";
+            @@COUNT += 1
     end
     if /\breturn\b/.match(input) 
-      toPrint += "\t * @return ${3:Variable Type}\n";
+      toPrint += "\t * @return ${" + @@COUNT.to_s + ":string}\n"
+      @@COUNT += 1
     end
     toPrint += "\t */\n"
     toPrint += input.gsub!('$', '\$')
